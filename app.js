963d127b4abe71e1106edb301825166aba2a666d
schismatiic <leonardojofre2003@gmail.com>
2026-08-21 23:15:39 -0400

    Some styles and views added

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
+// Router
+const indexRouter = require("./routes/indexRouter");
+
+// Load the .env port
 process.loadEnvFile();
 const PORT = process.env.PORT || 3000;
 
-app.use("/", (req, res) => {
-  res.send("Meaningless");
-});
+app.use("/", indexRouter);
 
 app.listen(PORT, (error) => {
   if (error) {
---
+++ routes/indexRouter.js
@@ -0,0 +1,41 @@
+const { Router } = require("express");
+const indexRouter = Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+      .toLocaleString("en-GB", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+      })
+      .replace(/\//g, "-")
+      .replace(",", " -"),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+      .toLocaleString("en-GB", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+      })
+      .replace(/\//g, "-")
+      .replace(",", " -"),
+  },
+];
+
+indexRouter.get("/", (req, res) => {
+  res.render("index", { messages: messages });
+});
+
+module.exports = indexRouter;
