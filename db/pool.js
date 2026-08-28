d9665c1b1f7236700e1285a8b6d3a319666a9bae
schismatiic <leonardojofre2003@gmail.com>
2026-08-27 21:16:24 -0400

    Some stuff that the db deploy will use

--- db/pool.js
+++ db/pool.js
@@ -3,9 +3,5 @@ const { Pool } = require("pg");
 process.loadEnvFile();
 
 module.exports = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DB_URL,
 });
